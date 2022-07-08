d86f7adeadfa590d05f6c41375ad7c6c970d35c4
vaibhavbohari <vaibhavbohari@gmail.com>
2022-07-08 09:05:26 +0530

    initial commit

---
+++ src/main/java/Facebook_Automation_Selenium/Facebook_Automation_Selenium/App.java
@@ -0,0 +1,13 @@
+package Facebook_Automation_Selenium.Facebook_Automation_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Facebook_Automation_Selenium/Facebook_Automation_Selenium/AppTest.java
@@ -0,0 +1,20 @@
+package Facebook_Automation_Selenium.Facebook_Automation_Selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
