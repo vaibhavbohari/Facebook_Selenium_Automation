3190397ee097a95a6be502dffbc639f1619b495c
vaibhavbohari <vaibhavbohari@gmail.com>
2022-07-08 10:19:06 +0530

    Screenshot_Browser

---
+++ src/test/java/com/bridgelabz/selenium/Screenshot.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.selenium;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+public class Screenshot {
+	
+	@Test
+	public void launchBrowser() throws IOException, InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\VAIBHAV\\QAAutomation\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+		driver.navigate().to("https://www.amazon.in/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		
+		TakesScreenshot ts =(TakesScreenshot) driver;
+		File srcFile = ts.getScreenshotAs(OutputType.FILE);
+		File destFile = new File("./Screenshot/"+ "amazon"+".png");
+		
+		
+		FileHandler.copy(srcFile, destFile);
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		driver.close();
+	}
+
+}
